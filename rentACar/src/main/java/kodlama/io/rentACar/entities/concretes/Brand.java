4a6b015a64b8efcb7b9dfc0d736b8cd1d1816eca
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-15 00:50:14 +0100

    Kodlama IO Java Camp - Day 5

--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,30 +1,22 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "brands")
+//@Data //Getter ve Setter ikisini getirir. @Getter ya da @Setter ile degistirilebilir
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String name;
-
-    public Brand() {
-    }
-
-    public Brand(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
