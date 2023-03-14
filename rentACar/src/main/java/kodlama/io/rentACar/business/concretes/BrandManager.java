e85b711bd6dd899f3ec1e5ffa665a9c468a09abd
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-15 00:50:33 +0100

    Kodlama IO Java Camp - Day 5

--- rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -1,11 +1,14 @@
 package kodlama.io.rentACar.business.concretes;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service //Bu sinif bir Business nesnesidir.
@@ -19,8 +22,29 @@ public class BrandManager implements BrandService {
     }
 
     @Override
-    public List<Brand> getAll() {
+    public List<GetAllBrandsResponse> getAll() {
+
+        List<Brand> brands = brandRepository.findAll();
+        List<GetAllBrandsResponse> brandResponse = new ArrayList<GetAllBrandsResponse>();
+
+        for (Brand brand : brands) {
+            GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
+            responseItem.setId(brand.getId());
+            responseItem.setName(brand.getName());
+
+            brandResponse.add(responseItem);
+        }
+
         //is kurallari
-        return brandRepository.getAll();
+        return brandResponse;
+    }
+
+    @Override
+    public void add(CreateBrandRequest createBrandRequest) {
+
+        Brand brand = new Brand();
+        brand.setName(createBrandRequest.getName());
+
+        this.brandRepository.save(brand);
     }
 }
--- rentACar/src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
+++ rentACar/src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -1,9 +1,9 @@
 package kodlama.io.rentACar.dataAccess.abstracts;
 
 import kodlama.io.rentACar.entities.concretes.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
 
-public interface BrandRepository {
-    List<Brand> getAll();
+
 }
