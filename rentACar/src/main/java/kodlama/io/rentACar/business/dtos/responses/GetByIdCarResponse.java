dc03acd600754f0e1fb69cf8fb4c5aa2dd558af0
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-19 03:36:03 +0100

    Kodlama IO Java Camp - Day 7

---
+++ rentACar/src/main/java/kodlama/io/rentACar/business/dtos/responses/GetByIdCarResponse.java
@@ -0,0 +1,17 @@
+package kodlama.io.rentACar.business.dtos.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetByIdCarResponse {
+
+    private int id;
+    private int modelYear;
+    private String modelName;
+    private Double dailyPrice;
+}
