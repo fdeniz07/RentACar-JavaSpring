c5eabe1540d51441ce6a1c25a24f65a35319a03c
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-19 03:35:48 +0100

    Kodlama IO Java Camp - Day 7

--- rentACar/src/main/java/kodlama/io/rentACar/business/dtos/requests/CreateCarRequest.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/dtos/requests/CreateCarRequest.java
@@ -10,10 +10,23 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateBrandRequest {
+public class CreateCarRequest {
+
+    @NotNull
+    @NotBlank
+    @Size(min=4,max = 4)
+    private int modelYear;
 
     @NotNull
     @NotBlank
     @Size(min=3,max = 20)
-    private String name;
+    private String modelName;
+
+    @NotNull
+    @NotBlank
+    private Double dailyPrice;
+
+    @NotNull
+    @NotBlank
+    private int modelId;
 }
--- rentACar/src/main/java/kodlama/io/rentACar/business/dtos/requests/CreateModelRequest.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/dtos/requests/CreateModelRequest.java
@@ -10,10 +10,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateBrandRequest {
+public class CreateModelRequest {
 
     @NotNull
     @NotBlank
     @Size(min=3,max = 20)
     private String name;
+
+    @NotNull
+    @NotBlank
+    private int brandId;
 }
