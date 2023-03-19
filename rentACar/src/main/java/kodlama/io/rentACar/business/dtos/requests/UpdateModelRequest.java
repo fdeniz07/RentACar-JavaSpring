d500531b94a556c52b697e34e1466576bc68e1e1
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-19 03:36:49 +0100

    Kodlama IO Java Camp - Day 7

---
+++ rentACar/src/main/java/kodlama/io/rentACar/business/dtos/requests/UpdateModelRequest.java
@@ -0,0 +1,23 @@
+package kodlama.io.rentACar.business.dtos.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateModelRequest {
+
+    @NotNull
+    @NotBlank
+    private int id;
+
+    @NotNull
+    @NotBlank
+    @Size(min=3,max = 20)
+    private String name;
+}
