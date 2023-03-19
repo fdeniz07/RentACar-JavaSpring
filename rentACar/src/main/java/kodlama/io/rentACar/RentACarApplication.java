c5c02f53a1c37cebc17906efeec2590b79c42dbc
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-19 03:36:36 +0100

    Kodlama IO Java Camp - Day 7

--- rentACar/src/main/java/kodlama/io/rentACar/RentACarApplication.java
+++ rentACar/src/main/java/kodlama/io/rentACar/RentACarApplication.java
@@ -1,23 +1,60 @@
 package kodlama.io.rentACar;
 
+import kodlama.io.rentACar.core.utilities.exceptions.BusinessException;
+import kodlama.io.rentACar.core.utilities.exceptions.ProblemDetails;
+import kodlama.io.rentACar.core.utilities.exceptions.ValidationProblemDetails;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
 
 @SpringBootApplication
+@RestControllerAdvice
+//Bu bir AOP annotation'unudur. Defensive Programming'de tüm hata kodlari son kullaniciya dökülmesin
 public class RentACarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentACarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarApplication.class, args);
+    }
 
-	@Bean //Bean annotation ile ben bu nesneyi IOC containere ekleyeyim, birisinin ihtiyaci oldugunda cagirir kullanirim der.
-	public ModelMapper getModelMapper(){
-		return new ModelMapper();
-	}
+    @ExceptionHandler //@RestControllerAdvice ile birlikte kullanilir. (Trick Noktasi)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ProblemDetails handleBusinessException(BusinessException businessException) { //class adinin önemi yok
+        ProblemDetails problemDetails = new ProblemDetails();
+        problemDetails.setMessage(businessException.getMessage());
+
+        return problemDetails;
+    }
+
+    @ExceptionHandler //@RestControllerAdvice ile birlikte kullanilir. (Trick Noktasi)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ProblemDetails handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException) { //class adinin önemi yok
+        ValidationProblemDetails validationProblemDetails = new ValidationProblemDetails();
+        validationProblemDetails.setMessage("VALIDATION.EXCEPTION");
+        validationProblemDetails.setValidationErrors(new HashMap<>());
+
+        for (FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()){
+            validationProblemDetails.getValidationErrors().put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        return validationProblemDetails;
+    }
+
+    @Bean
+    //Bean annotation ile ben bu nesneyi IOC containere ekleyeyim, birisinin ihtiyaci oldugunda cagirir kullanirim der.
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 }
 
 
 //Brand -->Marka
+//Model -->Model
 //Car -->Araba
